177b54ef29bfc2593a9e76bddb83be69c3db7c30
Tanushka12 <kashyaptanushka14@gmail.com>
2026-02-06 12:07:17 +0530

    feat: Initialize React project with Vite and ESLint
    - Added package.json with dependencies for React, ReactDOM, and development tools.
    - Created App component to display a greeting message.
    - Added Chai component to demonstrate component rendering.
    - Implemented main entry point for React application.
    - Configured Vite for React development.
    - Developed custom render function to simulate React-like rendering.
    - Created HTML template for custom React application.

---
+++ 01React/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./Chai"
+function App() {
+  const username = "Hi Tanushka!"
+  return (
+    <>
+      <Chai/>
+      <h1> Chai is Ready {username}</h1>
+    </>
+    
+  )
+} 
+
+export default App
---
+++ 01React/src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+function MyApp(){
+  return(
+    <div>
+      <h1>
+        Custom App !
+      </h1>
+    </div>
+  )
+}
+
+const ReactElement = {
+    type: 'a',
+    props:{
+        href: "https://google.com",
+        target: "_blank",
+
+    },
+    children: 'Click me to visit Google'
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'> Visit Google</a>
+)
+
+const anotherUser = " Hi Tanushka"
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit Google!',
+  anotherUser
+)
+
+createRoot(document.getElementById('root')).render(
+  reactElement
+)
